6c4ecaadc2311c80bbcf7780da86db92a3a1b958
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2022-10-06 10:14:00 +0900

    ログイン・ログアウトの切り替え

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,25 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
 
-const Logout = () => {
-	return <div>Logout</div>;
+const Logout = ({ setIsAuth }) => {
+	const navigate = useNavigate();
+
+	const logout = () => {
+		// ログアウト
+		signOut(auth).then(() => {
+			localStorage.clear();
+			setIsAuth(false);
+			navigate("/login");
+		});
+	};
+	return (
+		<div>
+			<p>ログアウトする</p>
+			<button onClick={logout}>ログアウト</button>
+		</div>
+	);
 };
 
 export default Logout;
